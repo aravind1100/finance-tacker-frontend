e45dcfb2f30c182d751ab95c72241e78f9fa3a7b
aravind1100 <aravindavd1999@gmail.com>
2025-11-20 22:23:54 +0530

    update VITE_URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}/api` })
+const API = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}/api` })
 
 //Attaching JWT token
 API.interceptors.request.use((req) => {
