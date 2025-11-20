0fe8eb3b14e14044b42915863c4af866f5569dfe
aravind1100 <aravindavd1999@gmail.com>
2025-11-20 19:35:50 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000/api" })
+const API = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}/api` })
 
 //Attaching JWT token
 API.interceptors.request.use((req) => {
